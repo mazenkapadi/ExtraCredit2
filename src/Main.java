cb40ae448388545118dccadb4b32d71537248ef9
mazenkapadi <73120392+mazenkapadi@users.noreply.github.com>
2023-04-28 22:53:09 -0400

    Initial Build

--- src/Main.java
+++ src/Main.java
@@ -15,16 +15,13 @@ public class Main {
         int max1 = 0;
         int max2 = 0;
 
-        for(int i = 0; i < a.length; i++)
-        {
+        for(int i = 0; i < a.length; i++) {
             int num = a[i];
-            if (max1 < num)
-            {
+            if (max1 < num) {
                 max2 = max1;
                 max1 = num;
             }
-            else if (max2 < num)
-            {
+            else if (max2 < num) {
                 max2 = num;
             }
         }
